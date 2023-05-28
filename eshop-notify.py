11697fb418d45546b9098589394e873fa2ed7895
wan <wanhuz96@gmail.com>
2023-05-28 16:09:04 +0800

    Add scheduler to repeat task

--- eshop-notify.py
+++ eshop-notify.py
@@ -3,6 +3,8 @@ from module.eshop_notifier_email import *
 from module.eshop_prices_check import *
 from module.eshop_prices_extension import *
 import sys
+import schedule
+import time
 
 # Read environment file
 try:
@@ -18,33 +20,44 @@ try:
                     RECEIVER_EMAIL.append(receiver_email.replace('\'', ''))
             elif (line[0] == 'SENDER_PASS'):
                 SENDER_PASS = line[2].replace('\'', '')
+            elif (line[0] == 'CHECK_PRICE_EVERY_X_MINUTE'):
+                MINUTE_TO_REPEAT = line[2].replace('\'', '')
+                MINUTE_TO_REPEAT = int(MINUTE_TO_REPEAT)
 except IOError:
     sys.exit('Environment file could not be found. Create env file using env_example.txt')
 
+# Create main job
+def job():
+    eshop_notifier_db = EshopNotifierDB('config/watchlist.txt', 'config/pricelist.json')
+    eshop_notifier_db.init_db()
 
-# Program execution starts here
-eshop_notifier_db = EshopNotifierDB('config/watchlist.txt', 'config/pricelist.json')
-eshop_notifier_db.init_db()
+    eshop_price_checker = EshopPricesCheck(eshop_notifier_db.db, eshop_notifier_db.db_query, 'config/watchlist.txt')
+    game_with_discount = eshop_price_checker.get_game_with_discount()
 
-eshop_price_checker = EshopPricesCheck(eshop_notifier_db.db, eshop_notifier_db.db_query, 'config/watchlist.txt')
-game_with_discount = eshop_price_checker.get_game_with_discount()
+    eshop_prices_extension = EshopPricesExtension()
+    eshop_emailer = EshopNotifierEmail(SENDER_EMAIL, RECEIVER_EMAIL, SENDER_PASS)
 
-eshop_prices_extension = EshopPricesExtension()
-eshop_emailer = EshopNotifierEmail(SENDER_EMAIL, RECEIVER_EMAIL, SENDER_PASS)
+    for game_url in game_with_discount:
+        game_details = eshop_prices_extension.get_game_details(game_url)
+        eshop_notifier_db.record_price(game_url, game_details['current_price'], eshop_notifier_db.db)
 
-for game_url in game_with_discount:
-    game_details = eshop_prices_extension.get_game_details(game_url)
-    eshop_notifier_db.record_price(game_url, game_details['current_price'], eshop_notifier_db.db)
+        with open('email_template.html', 'r') as email_template:
+            message = eshop_emailer.compose_message(email_template.read(), 
+                                game_details['game_url'],
+                                game_details['game_image_url'],
+                                game_details['original_price'],
+                                game_details['current_price'], 
+                                game_details['game_desc'],
+                                "RM")
 
-    with open('email_template.html', 'r') as email_template:
-        message = eshop_emailer.compose_message(email_template.read(), 
-                            game_details['game_url'],
-                            game_details['game_image_url'],
-                            game_details['original_price'],
-                            game_details['current_price'], 
-                            game_details['game_desc'],
-                            "RM")
+            subject = eshop_emailer.compose_subject(game_details['game_title'])
 
-        subject = eshop_emailer.compose_subject(game_details['game_title'])
+            eshop_emailer.send_email(message, subject)
 
-        eshop_emailer.send_email(message, subject)
+
+# Create scheduler to repeat script
+schedule.every(MINUTE_TO_REPEAT).minutes.do(job)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
